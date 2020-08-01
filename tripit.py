72db3d69a734b78ad7a9b2a48c1b4e83a3fbd849
Michal Chruszcz <michal@iwoca.co.uk>
2020-08-02 00:32:57 +0200

    Fixed date and time parsing.

--- tripit.py
+++ tripit.py
@@ -214,11 +214,11 @@ class ResponseHandler(xml.sax.handler.ContentHandler):
 
     def endElement(self, name):
         if self._current_content is not None:
-            if name.endswith('date'):
+            if name.endswith('_date'):
                 self._current_content = datetime.date(
                     *(time.strptime(
                         self._current_content, '%Y-%m-%d')[0:3]))
-            elif name.endswith('time'):
+            elif name.endswith('_time'):
                 self._current_content = datetime.time(
                     *(time.strptime(
                         self._current_content, '%H:%M:%S')[3:6]))
